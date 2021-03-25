64259a3d77b2d0267785ead15e8b8da0c29f7d21
baigyanik <kundan1648@gmail.com>
2021-03-25 17:30:13 -0400

    start trying to plot the rms stuff at different locations; rename rms to rms_cycle; convert list of folder names into floats;

--- rms_all_locations.py
+++ rms_all_locations.py
@@ -7,10 +7,16 @@ import numpy as np
 file_names = ['12', '14', '16', '18', '20', '22', '24', '26', '28', '30']
 N_files = len(file_names)
 
+# also convert the list into an array of floats
+file_names_float = np.zeros(N_files)
+for i in range(N_files):
+    file_names_float[i] = float(file_names[i])
+
 # choose trajectory name for which to process data
 trajectory_name = '30deg'
 
 # I might have to use a for loop (for ease) at each location, since the length of data at each location might differ by a small number
+
 
 # %%
 # for each file, I need 6 components of rms ft
@@ -25,23 +31,7 @@ for k in range(N_files):
     ang_meas = np.loadtxt(file_names[k] + '/' + trajectory_name + '/' + 'ang_meas.csv', delimiter=',', unpack=True)
     cpg_param = np.loadtxt(file_names[k] + '/' + trajectory_name + '/' + 'cpg_param.csv', delimiter=',', unpack=True)
 
-    # %%
     N = len(cpg_param[1])  # number of data points
-
-    ####################### WILL BE OBSOLETE ########################
-    # # find points where new cpg parameters are used
-    # cpg_param_change = cpg_param[:, 0:(N-1)] - cpg_param[:, 1:N]
-    # cpg_param_change_idx = np.nonzero(cpg_param_change)[1] + 1
-    # # [1] means take the 2nd input of the nonzero() fcn, which gives where nonzero input by column
-    # # +1 needed so idx is for start of new param set, rather than end of old param set
-
-    # # test = cpg_param[:, cpg_param_change_idx[1] - 1]  # checking if the cpg params really change at the index
-
-    # cpg_param_change_idx = np.insert(cpg_param_change_idx, 0, 0)  # including the first one
-    # cpg_param_change_idx = cpg_param_change_idx.astype(int)
-
-    # N_param = len(cpg_param_change_idx)  # number of param sets
-    #################################################################
 
     # find points where a new stroke cycle is started
     t_s = round(t[1] - t[0], 3)  # sample time
@@ -64,12 +54,11 @@ for k in range(N_files):
     print('Number of unused data points:')
     print((t_param / t_s).astype(int) - N_per_cycle * N_cycles)  # total # of data points - # of data points used
 
-    # %%
     # 3d matrix, 1st dim is a param set, 2nd dim is stroke cycle, 3rd dim are the 6 FT
     # need to have same number of cycles for each param set
     # at the same time, also do the same for the magnitude of FT instead of the 3 directions separately
-    rms = np.zeros((N_cycles, 6))
-    rms_norm = np.zeros((N_cycles, 2))
+    rms_cycle = np.zeros((N_cycles, 6))
+    rms_norm_cycle = np.zeros((N_cycles, 2))
 
     # calculate RMS values for each FT, for each stroke cycle, for each param set
     for j in range(N_cycles):
@@ -80,10 +69,17 @@ for k in range(N_files):
         T_meas_norm_cycle = np.linalg.norm(ft_meas_cycle[3:6, :], axis=0)
 
         # rms
-        rms[j, :] = np.sqrt(1/N_per_cycle * np.sum(ft_meas_cycle**2, axis=1))
-        rms_norm[j, 0] = np.sqrt(1/N_per_cycle * np.sum(f_meas_norm_cycle**2))
-        rms_norm[j, 1] = np.sqrt(1/N_per_cycle * np.sum(T_meas_norm_cycle**2))
+        rms_cycle[j, :] = np.sqrt(1/N_per_cycle * np.sum(ft_meas_cycle**2, axis=1))
+        rms_norm_cycle[j, 0] = np.sqrt(1/N_per_cycle * np.sum(f_meas_norm_cycle**2))
+        rms_norm_cycle[j, 1] = np.sqrt(1/N_per_cycle * np.sum(T_meas_norm_cycle**2))
 
     # average the FT RMS for across all stroke cycles
-    rms_all[k, :] = np.mean(rms, axis=0)
-    rms_norm_all[k, :] = np.mean(rms_norm, axis=0)
+    rms_all[k, :] = np.mean(rms_cycle, axis=0)
+    rms_norm_all[k, :] = np.mean(rms_norm_cycle, axis=0)
+
+# %%
+import matplotlib.pyplot as plt
+
+for i in range(6):
+    plt.figure()
+    plt.plot()
