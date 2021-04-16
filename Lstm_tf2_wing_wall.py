bc2ce197f5b9a999a278b946ca1420412eec91a0
baigyanik <kundan1648@gmail.com>
2021-04-16 02:06:39 -0400

    make grouping number of stroke cycles and overlaps adjustable; something is not right;

--- Lstm_tf2_wing_wall.py
+++ Lstm_tf2_wing_wall.py
@@ -5,10 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 # from correct_biases import correct_biases
 from tensorflow import keras
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.math import confusion_matrix
 
 # %%
 # all files to extract the data from (collected at multiple locations)
-file_names = ['6', '12']
+file_names = ['0', '6', '12', '18']
 N_files = len(file_names)
 
 # also convert the list into an array of floats
@@ -39,35 +41,34 @@ t_total = np.around(t_total, decimals=3)
 
 # calculate number of data points per cycle
 N_per_cycle = round(t_cycle / t_s)
-
-print('Number of data points in a cycle:')
-print(N_per_cycle)
+print('Data points in a cycle:', N_per_cycle)
 
 # N_cycles = 50
 N_cycles = floor(N / N_per_cycle)  # floor(total data points / data points in a cycle)
-print('Number of stroke cycles:')
-print(N_cycles)
+print('Stroke cycles:', N_cycles)
+print('Unused data points:', N - N_per_cycle * N_cycles)  # print number of unused data points
 
-# print number of unused data points
-print('Number of unused data points:')
-print(N - N_per_cycle * N_cycles)  # total # of data points - # of data points used
+# group cycles together
+N_cycles_example = 4  # use this number of stroke cycles as 1 example
+N_cycles_overlap = 1  # number of overlapping cycles between consecutive examples
+N_examples = (N_cycles - N_cycles_example) // N_cycles_overlap + 1  # int division
+print('Total examples:', N_examples)
 
 # number of training and testing stroke cycles
-N_cycles_train = round(0.8 * N_cycles)
-N_cycles_test = N_cycles - N_cycles_train
-print('Number of training stroke cycles:')
-print(N_cycles_train)
-print('Number of testing stroke cycles:')
-print(N_cycles_test)
+N_examples_train = round(0.8 * N_examples)
+N_examples_test = N_examples - N_examples_train
+print('Training examples:', N_examples_train)
+print('Testing examples:', N_examples_test)
+
+N_inputs = 7
+print('Inputs:', N_inputs)
 
 # %%
-N_inputs = 7
-
-data = np.zeros((N_files * N_cycles * N_per_cycle, N_inputs))  # all data
-x = np.zeros((N_files * N_cycles_train, N_inputs, N_per_cycle))
-y = np.zeros((N_files * N_cycles_train))
-x_val = np.zeros((N_files * N_cycles_test, N_inputs, N_per_cycle))
-y_val = np.zeros((N_files * N_cycles_test))
+data = np.zeros((N_files * N_examples * N_cycles_example * N_per_cycle, N_inputs))  # all data, with groups of stroke cycles
+x = np.zeros((N_files * N_examples_train, N_inputs, N_cycles_example * N_per_cycle))
+y = np.zeros((N_files * N_examples_train))
+x_val = np.zeros((N_files * N_examples_test, N_inputs, N_cycles_example * N_per_cycle))
+y_val = np.zeros((N_files * N_examples_test))
 
 ############### alternative nominal FT - futhest from wall ###############
 ft_pred = np.loadtxt('22/' + trajectory_name + '/' + 'ft_meas.csv', delimiter=',', unpack=True)
@@ -81,31 +82,35 @@ for k in range(N_files):
     cpg_param = np.loadtxt(file_names[k] + '/' + trajectory_name + '/' + 'cpg_param.csv', delimiter=',', unpack=True)
 
     ############### take difference?? ###############
-    ft_meas -= ft_pred
+    # ft_meas -= ft_pred
 
-    data[(k * N_cycles * N_per_cycle):((k+1) * N_cycles * N_per_cycle), 0:6] = ft_meas[:, 0:(N_cycles * N_per_cycle)].T  # meassured FT
-    data[(k * N_cycles * N_per_cycle):((k+1) * N_cycles * N_per_cycle), 6] = ang_meas[0, 0:(N_cycles * N_per_cycle)].T  # stroke angle
+    for i in range(N_examples):
+        data[((k*N_examples + i) * N_cycles_example * N_per_cycle):((k*N_examples + i + 1) * N_cycles_example * N_per_cycle), 0:6] = \
+            ft_meas[:, (i*N_cycles_overlap * N_per_cycle):((i*N_cycles_overlap + N_cycles_example) * N_per_cycle)].T  # measured FT
+        data[((k*N_examples + i) * N_cycles_example * N_per_cycle):((k*N_examples + i + 1) * N_cycles_example * N_per_cycle), 6] = \
+            ang_meas[0, (i*N_cycles_overlap * N_per_cycle):((i*N_cycles_overlap + N_cycles_example) * N_per_cycle)].T  # stroke angle
 
+# %%
 data = (data - np.min(data, axis=0)) / (np.max(data, axis=0) - np.min(data, axis=0))  # normalize
-ft_meas_norm = data.reshape(N_files * N_cycles, N_per_cycle, N_inputs)
-ft_meas_norm = ft_meas_norm.transpose(0, 2, 1)  # cycle -> FT components -> all data points of that cycle
+
+ft_meas_norm = data.reshape(N_files * N_examples, N_cycles_example * N_per_cycle, N_inputs)
+ft_meas_norm = ft_meas_norm.transpose(0, 2, 1)  # example -> FT components -> all data points of that example
 
 # split data into training and testing sets
 for k in range(N_files):
-    x[N_cycles_train*k:N_cycles_train*(k+1), :, :] = ft_meas_norm[N_cycles*k:N_cycles*(k+1)-N_cycles_test, :, :]
-    y[N_cycles_train*k:N_cycles_train*(k+1)] = k
-    x_val[N_cycles_test*k:N_cycles_test*(k+1), :, :] = ft_meas_norm[N_cycles*k+N_cycles_train:N_cycles*(k+1), :, :]
-    y_val[N_cycles_test*k:N_cycles_test*(k+1)] = k
+    x[k*N_examples_train:(k+1)*N_examples_train, :, :] = ft_meas_norm[k*N_examples:(k+1)*N_examples - N_examples_test, :, :]
+    y[k*N_examples_train:(k+1)*N_examples_train] = k  # class = file
+    x_val[k*N_examples_test:(k+1)*N_examples_test, :, :] = ft_meas_norm[k*N_examples + N_examples_train:(k+1)*N_examples, :, :]
+    y_val[k*N_examples_test:(k+1)*N_examples_test] = k  # class = file
 
 # %%
 model = keras.models.Sequential(
     [
-        keras.layers.RNN(keras.layers.LSTMCell(128), return_sequences=True, input_shape=(N_inputs, N_per_cycle)),
+        keras.layers.RNN(keras.layers.LSTMCell(128), return_sequences=True, input_shape=(N_inputs, N_cycles_example * N_per_cycle)),
         keras.layers.RNN(keras.layers.LSTMCell(128)),
-        keras.layers.Dense(2),
+        keras.layers.Dense(N_files, activation='sigmoid')
     ]
 )
-
 
 model.compile(
     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -115,13 +120,28 @@ model.compile(
 
 # model.summary()
 
-# print("Learning rate:", model.optimizer.learning_rate.numpy())
-lr = 0.002
+lr = 0.001
 keras.backend.set_value(model.optimizer.learning_rate, lr)
 # print("Learning rate:", model.optimizer.learning_rate.numpy())
 
+early_stopping_monitor = EarlyStopping(
+    monitor='val_accuracy',
+    min_delta=0,
+    patience=100,
+    verbose=0,
+    mode='auto',
+    baseline=None,
+    restore_best_weights=True
+)
+
 history = model.fit(
-    x, y, validation_data=(x_val, y_val), epochs=1000, verbose=0
+    x, y,
+    validation_data=(x_val, y_val),
+    epochs=1000,
+    verbose=0,
+    callbacks=[early_stopping_monitor],
+    shuffle=False,
+    use_multiprocessing=False
 )
 
 plt.plot(history.history['accuracy'])
@@ -131,7 +151,11 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 
-plt.savefig('plots/2021.04.11/' + trajectory_name + '/lstm_filtered_' + str(file_names) + '_' + str(lr) + '.png')  # change this
+# plt.savefig('plots/2021.04.11/' + trajectory_name + '/lstm_filtered_' + str(file_names) + '_' + str(lr) + '.png')  # change this
 plt.show()
+
+# %%
+print(confusion_matrix(y, np.argmax(model.predict(x), axis=-1)))
+print(confusion_matrix(y_val, np.argmax(model.predict(x_val), axis=-1)))
 
 # %%
