db5e7c906bffe0cd7b97a8b1db8a1353cf59b723
baigyanik <kundan1648@gmail.com>
2021-04-20 14:16:39 -0400

    make number of cells also a parameter;

--- Lstm_tf2_wing_wall.py
+++ Lstm_tf2_wing_wall.py
@@ -7,7 +7,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.math import confusion_matrix
 
 # %% design parameters
-file_names = ['6', '18']
+file_names = ['0', '6', '12', '18']
 file_names_offset = 3  # difference in between actual distance and file names
 trajectory_name = '30deg'  # choose trajectory name for which to process data
 
@@ -21,13 +21,14 @@ subract_prediction = False
 
 shuffle_examples = False
 
+cells_number = 64  # number of lstm cells of each lstm layer
 lr = 0.02  # learning rate
 epochs_number = 1000  # number of epochs
 epochs_patience = 1000  # number of epochs of no improvement after which training is stopped
 
 save_plot = True
-save_cm = False
-save_folder = 'plots/2021.04.16'
+save_cm = True
+save_folder = 'plots/2021.04.20_multi-class'
 
 # %%
 # all files to extract the data from (collected at multiple locations)
@@ -119,9 +120,10 @@ for k in range(N_files):
 # %%
 model = keras.models.Sequential(
     [
-        keras.layers.RNN(keras.layers.LSTMCell(128), return_sequences=True, input_shape=(N_inputs, N_cycles_example * N_per_cycle)),
-        # keras.layers.RNN(keras.layers.LSTMCell(128), return_sequences=True),
-        keras.layers.RNN(keras.layers.LSTMCell(128)),
+        keras.layers.RNN(keras.layers.LSTMCell(cells_number), return_sequences=True, input_shape=(N_inputs, N_cycles_example * N_per_cycle)),
+        # keras.layers.RNN(keras.layers.LSTMCell(cells_number), return_sequences=True),
+        keras.layers.RNN(keras.layers.LSTMCell(cells_number)),
+        # keras.layers.Dense(cells_number),
         keras.layers.Dense(N_files, activation='softmax')
     ]
 )
@@ -167,10 +169,10 @@ cm_train = confusion_matrix(y, np.argmax(model.predict(x), axis=-1))
 cm_test = confusion_matrix(y_val, np.argmax(model.predict(x_val), axis=-1))
 
 if save_plot:
-    plt.savefig(save_folder + '/lstm_' + str(file_names) + '_(' + str(N_cycles_example) + ',' + str(N_cycles_step) + ')_' + str(lr) + '.png')
+    plt.savefig(save_folder + '/lstm_' + str(file_names) + '_(' + str(N_cycles_example) + ',' + str(N_cycles_step) + ')_3layer' + str(cells_number) + '_' + str(lr) + '.png')
 if save_cm:
-    np.savetxt(save_folder + '/lstm_' + str(file_names) + '_(' + str(N_cycles_example) + ',' + str(N_cycles_step) + ')_' + str(lr) + '_train.txt', cm_train, fmt='%d')
-    np.savetxt(save_folder + '/lstm_' + str(file_names) + '_(' + str(N_cycles_example) + ',' + str(N_cycles_step) + ')_' + str(lr) + '_test.txt', cm_test, fmt='%d')
+    np.savetxt(save_folder + '/lstm_' + str(file_names) + '_(' + str(N_cycles_example) + ',' + str(N_cycles_step) + ')_3layer' + str(cells_number) + '_' + str(lr) + '_train.txt', cm_train, fmt='%d')
+    np.savetxt(save_folder + '/lstm_' + str(file_names) + '_(' + str(N_cycles_example) + ',' + str(N_cycles_step) + ')_3layer' + str(cells_number) + '_' + str(lr) + '_test.txt', cm_test, fmt='%d')
 
 print(cm_train)
 print(cm_test)
