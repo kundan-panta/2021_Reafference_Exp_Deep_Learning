f646bb1e3e96ec64385c1978073fec120733cc99
kundan-panta <kundan1648@gmail.com>
2021-06-11 09:25:40 -0400

    make axis limits the same;

--- plot_all_locations.py
+++ plot_all_locations.py
@@ -10,16 +10,16 @@ data_folder = 'data/2021.05.25/filtered_a5_s10_o60/'  # include trailing slash
 file_names_offset = 2  # difference in between actual distance and file names
 # file_names = ['0', '3', '6', '6_2', '9', '12', '15', '18', '21', '24']
 # file_distances = np.array([0, 3, 6, 6, 9, 12, 15, 18, 21, 24], dtype=float) + file_names_offset
-file_names = ['12-1', '12-3', '12-4', '12-5', '12-6', '12-7', '12-8', '12-9', '12-10', '12-11']
-file_distances = np.array([14, 14, 14, 14, 14, 14, 14, 14, 14, 14], dtype=float) + file_names_offset
+file_names = ['3-2', '3-3']  # , '12-4', '12-5', '12-6', '12-7', '12-8', '12-9', '12-10', '12-11']
+file_distances = np.array([3, 3], dtype=float) + file_names_offset
 trajectory_name = '30deg'  # choose trajectory name for which to process data
 
 empirical_prediction = True  # whether to use collected data as the "perfect prediction"
 empirical_prediction_name = '24-1'
 subract_prediction = False  # meas - pred?
 
-save_folder = root_folder + 'plots/2021.05.29_dataplots/'  # include trailing slash
-save_filename = '14_f5,10,60_'
+save_folder = root_folder + 'plots/2021.06.11_dataplots/'  # include trailing slash
+save_filename = '3_f5,10,60_'  # include traliing underscore
 
 plot_size = (18, 12)
 subplot_grid_shape = [4, 3]
@@ -62,42 +62,60 @@ for k in range(N_files):
 
     # a subplot for each location
     plt.figure(1)
-    plt.subplot(*subplot_grid_shape, k+1)
+    if k == 0:  # make axis limits the same
+        ax1 = plt.subplot(*subplot_grid_shape, k+1)
+    else:
+        plt.subplot(*subplot_grid_shape, k+1, sharex=ax1, sharey=ax1)
     plt.xlabel('Time (s) (' + str(file_distances[k]) + ' cm)')
     plt.ylabel('Force X (Measured) (N)')
     plt.plot(t[idx_start:idx_end], ft_meas[0, idx_start:idx_end])
     plt.plot(t[idx_start:idx_end], ang_meas[0, idx_start:idx_end]*0.01, ':')  # superpose with stroke angle
 
     plt.figure(2)
-    plt.subplot(*subplot_grid_shape, k+1)
+    if k == 0:  # make axis limits the same
+        ax2 = plt.subplot(*subplot_grid_shape, k+1)
+    else:
+        plt.subplot(*subplot_grid_shape, k+1, sharex=ax2, sharey=ax2)
     plt.xlabel('Time (s) (' + str(file_distances[k]) + ' cm)')
     plt.ylabel('Force Y (Measured) (N)')
     plt.plot(t[idx_start:idx_end], ft_meas[1, idx_start:idx_end])
     plt.plot(t[idx_start:idx_end], ang_meas[0, idx_start:idx_end]*0.001, ':')  # superpose with stroke angle
 
     plt.figure(3)
-    plt.subplot(*subplot_grid_shape, k+1)
+    if k == 0:  # make axis limits the same
+        ax3 = plt.subplot(*subplot_grid_shape, k+1)
+    else:
+        plt.subplot(*subplot_grid_shape, k+1, sharex=ax3, sharey=ax3)
     plt.xlabel('Time (s) (' + str(file_distances[k]) + ' cm)')
     plt.ylabel('Force Z (Measured) (N)')
     plt.plot(t[idx_start:idx_end], ft_meas[2, idx_start:idx_end])
     plt.plot(t[idx_start:idx_end], ang_meas[0, idx_start:idx_end]*0.001, ':')  # superpose with stroke angle
 
     plt.figure(4)
-    plt.subplot(*subplot_grid_shape, k+1)
+    if k == 0:  # make axis limits the same
+        ax4 = plt.subplot(*subplot_grid_shape, k+1)
+    else:
+        plt.subplot(*subplot_grid_shape, k+1, sharex=ax4, sharey=ax4)
     plt.xlabel('Time (s) (' + str(file_distances[k]) + ' cm)')
     plt.ylabel('Torque X (Measured) (N-mm)')
     plt.plot(t[idx_start:idx_end], ft_meas[3, idx_start:idx_end])
     plt.plot(t[idx_start:idx_end], ang_meas[0, idx_start:idx_end]*0.25, ':')  # superpose with stroke angle
 
     plt.figure(5)
-    plt.subplot(*subplot_grid_shape, k+1)
+    if k == 0:  # make axis limits the same
+        ax5 = plt.subplot(*subplot_grid_shape, k+1)
+    else:
+        plt.subplot(*subplot_grid_shape, k+1, sharex=ax5, sharey=ax5)
     plt.xlabel('Time (s) (' + str(file_distances[k]) + ' cm)')
     plt.ylabel('Torque Y (Measured) (N-mm)')
     plt.plot(t[idx_start:idx_end], ft_meas[4, idx_start:idx_end])
     plt.plot(t[idx_start:idx_end], ang_meas[0, idx_start:idx_end]*0.5, ':')  # superpose with stroke angle
 
     plt.figure(6)
-    plt.subplot(*subplot_grid_shape, k+1)
+    if k == 0:  # make axis limits the same
+        ax6 = plt.subplot(*subplot_grid_shape, k+1)
+    else:
+        plt.subplot(*subplot_grid_shape, k+1, sharex=ax6, sharey=ax6)
     plt.xlabel('Time (s) (' + str(file_distances[k]) + ' cm)')
     plt.ylabel('Torque Z (Measured) (N-mm)')
     plt.plot(t[idx_start:idx_end], ft_meas[5, idx_start:idx_end])
