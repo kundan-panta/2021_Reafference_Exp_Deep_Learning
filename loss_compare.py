10538cbd9d94ed07faca845e0b27fe1fa8d73d51
kundan-panta <kundan.panta123@gmail.com>
2022-04-17 18:36:20 -0400

    add notches to boxplots; allow choosing between plotting wingtip or wingroot to wall distance;

--- loss_compare.py
+++ loss_compare.py
@@ -14,10 +14,10 @@ losses_std = np.full((len(Ro_all), len(A_star_all)), np.NaN)
 
 # assume there's only 1 folder for each case
 root_folder = ''  # include trailing slash
-save_folders = ['plots/2022.03.25_exp_best_sh=5/', 'plots/2022.03.25_exp_best_sh=50/', 'plots/2022.03.25_exp_best_sh=500/', 'plots/2022.04.14_exp_sh=5000/']  # include trailing slash
+save_folders = ['plots/2022.03.25_exp_best_sh=5/', 'plots/2022.03.25_exp_best_sh=50/', 'plots/2022.03.25_exp_best_sh=500/', 'plots/2022.04.14_exp_sh=5000/', 'plots/2022.04.16_exp_sh=50000/']  # include trailing slash
 # save_folders = ['plots/2022.03.26_exp_best_same_sensor_loc_sh=500/']  # include trailing slash
 save_folders = [root_folder + save_folder for save_folder in save_folders]
-plot_folder = root_folder + 'plots/2022.04.15_loss_plots/'
+plot_folder = root_folder + 'plots/2022.04.17_loss_plots/'
 
 # %% mean and std plots
 
@@ -67,12 +67,16 @@ def loss_mean_std(losses_case):
 # plt.ylabel('Test Set Loss')
 
 # %% boxplot
+# # distances between which to get losses
+# d_min = 0
+# d_max = 99
+
 # ylim = [0, 8]
 # plt.tight_layout()
 
 # fig = plt.figure(figsize=[16, 4])
 # fig.supxlabel('Ro')
-# fig.supylabel('Mean Absolute Error (cm)')
+# fig.supylabel('|Prediction Error| (cm)')
 
 # for A_star_ind, A_star in enumerate(A_star_all):
 #     losses_boxplot = []
@@ -80,14 +84,14 @@ def loss_mean_std(losses_case):
 #         losses_boxplot.append(load_loss(Ro, A_star, d_min, d_max))
 
 #     plt.subplot(1, 3, A_star_ind + 1)
-#     plt.boxplot(losses_boxplot, showfliers=False)
+#     plt.boxplot(losses_boxplot, showfliers=False, notch=True)
 #     plt.xticks([1, 2, 3], Ro_all)
 #     plt.title('A* = {}'.format(A_star))
-#     # plt.ylim(ylim)
+#     plt.ylim(ylim)
 
 # fig = plt.figure(figsize=[16, 4])
 # fig.supxlabel('A*')
-# fig.supylabel('Mean Absolute Error (cm)')
+# fig.supylabel('|Prediction Error| (cm)')
 
 # for Ro_ind, Ro in enumerate(Ro_all):
 #     losses_boxplot = []
@@ -95,15 +99,12 @@ def loss_mean_std(losses_case):
 #         losses_boxplot.append(load_loss(Ro, A_star, d_min, d_max))
 
 #     plt.subplot(1, 3, Ro_ind + 1)
-#     plt.boxplot(losses_boxplot, showfliers=False)
+#     plt.boxplot(losses_boxplot, showfliers=False, notch=True)
 #     plt.xticks([1, 2, 3], A_star_all)
 #     plt.title('Ro = {}'.format(Ro))
-#     # plt.ylim(ylim)
+#     plt.ylim(ylim)
 
 # %% make loss plots with multiple rows for each distance
-# create folder to save plots to
-Path(plot_folder).mkdir(parents=True, exist_ok=True)
-
 # d_rows = [[1, 1], [4, 4], [7, 7], [10, 10], [13, 13], [16, 16], [19, 19], [22, 22], [25, 25], [28, 28], [31, 31], [34, 34], [37, 37], [40, 40]]
 # d_rows = [[1, 4], [7, 10], [13, 16], [19, 22], [25, 28], [31, 34], [37, 40]]
 # d_rows = [[1, 7], [10, 16], [19, 25], [28, 34], [37, 40]]
@@ -149,16 +150,33 @@ Path(plot_folder).mkdir(parents=True, exist_ok=True)
 
 # plt.ylim([0, 10])
 
-# with distance on x-axis
+# %% with distance on x-axis
+# create folder to save plots to
+Path(plot_folder).mkdir(parents=True, exist_ok=True)
+
 d_all = [list(range(1, 40 + 1, 3))] * len(Ro_all)
 # d_all = [list(range(10, 46 + 1, 3)), list(range(4, 40 + 1, 3)), list(range(1, 37 + 1, 3))]  # if using same sensor-to-wall distance
 
-# find distance from opposite wall of tank
+# find wingroot-to-wall distance from opposite wall of tank
 wing_len_Ro = [12.367, 17.059, 20.827]
 tank_len = 81  # tank length (cm)
 d_all_opp = []
 for Ro_ind in range(len(d_all)):
     d_all_opp.append([round(tank_len - (wing_len_Ro[Ro_ind] + d), 1) for d in d_all[Ro_ind]])
+
+# use wingroot-to-wall instead of wingtip-to-wall in the plot
+sensor_to_wall_distance = True
+
+if sensor_to_wall_distance:
+    # calculate the wingroot-to-wall distance
+    d_all_label = []
+    for Ro_ind in range(len(d_all)):
+        d_all_label.append([round(wing_len_Ro[Ro_ind] + d, 1) for d in d_all[Ro_ind]])
+    # don't share the x-axis when comparing different Ro b/c the wingroot-to-wall distances will be different
+    sharex = False
+else:
+    d_all_label = d_all
+    sharex = True
 
 # put the Ro's close together
 figs_A_star = [0] * len(A_star_all)
@@ -169,20 +187,20 @@ axs_opp_Ro = [[0] * len(Ro_all)] * len(A_star_all)
 color_opp = 'green'
 
 for i in range(len(A_star_all)):
-    figs_A_star[i], axs_A_star[i] = plt.subplots(len(Ro_all), 1, sharex=True, sharey=True, figsize=(15, 10))
+    figs_A_star[i], axs_A_star[i] = plt.subplots(len(Ro_all), 1, sharex=sharex, sharey=True, figsize=(15, 10))
 
 for A_star_ind, A_star in enumerate(A_star_all):
     figs_A_star[A_star_ind].suptitle('A* = {}'.format(A_star))
     figs_A_star[A_star_ind].supxlabel('Distance (cm)')
-    figs_A_star[A_star_ind].supylabel('MAE (cm)')
+    figs_A_star[A_star_ind].supylabel('|Prediction Error| (cm)')
 
     for Ro_ind, Ro in enumerate(Ro_all):
         losses_boxplot = []
         for d_ind, d in enumerate(d_all[Ro_ind]):  # one boxplot for each distance
             losses_boxplot.append(load_loss(Ro, A_star, d, d))
 
-        axs_A_star[A_star_ind][Ro_ind].boxplot(losses_boxplot, showfliers=False)
-        plt.setp(axs_A_star[A_star_ind][Ro_ind], xticks=list(range(1, len(d_all[Ro_ind]) + 1)), xticklabels=d_all[Ro_ind])
+        axs_A_star[A_star_ind][Ro_ind].boxplot(losses_boxplot, showfliers=False, notch=True)
+        plt.setp(axs_A_star[A_star_ind][Ro_ind], xticks=list(range(1, len(d_all[Ro_ind]) + 1)), xticklabels=d_all_label[Ro_ind])
         axs_A_star[A_star_ind][Ro_ind].set_ylim(0, 10)
         axs_A_star[A_star_ind][Ro_ind].set_ylabel('Ro = {}'.format(Ro))
 
@@ -207,7 +225,7 @@ for A_star_ind, A_star in enumerate(A_star_all):
 # put the A*'s close together
 figs_Ro = [0] * len(Ro_all)
 axs_Ro = [0] * len(Ro_all)
-ylim_Ro = [2, 10, 10]
+ylim_Ro = [10, 10, 10]
 
 # for opposite wall distance
 axs_opp_Ro = [[0] * len(Ro_all)] * len(A_star_all)
@@ -219,15 +237,15 @@ for i in range(len(Ro_all)):
 for Ro_ind, Ro in enumerate(Ro_all):
     figs_Ro[Ro_ind].suptitle('Ro = {}'.format(Ro))
     figs_Ro[Ro_ind].supxlabel('Distance (cm)')
-    figs_Ro[Ro_ind].supylabel('MAE (cm)')
+    figs_Ro[Ro_ind].supylabel('|Prediction Error| (cm)')
 
     for A_star_ind, A_star in enumerate(A_star_all):
         losses_boxplot = []
         for d_ind, d in enumerate(d_all[Ro_ind]):  # one boxplot for each distance
             losses_boxplot.append(load_loss(Ro, A_star, d, d))
 
-        axs_Ro[Ro_ind][A_star_ind].boxplot(losses_boxplot, showfliers=False)
-        plt.setp(axs_Ro[Ro_ind][A_star_ind], xticks=list(range(1, len(d_all[Ro_ind]) + 1)), xticklabels=d_all[Ro_ind])
+        axs_Ro[Ro_ind][A_star_ind].boxplot(losses_boxplot, showfliers=False, notch=True)
+        plt.setp(axs_Ro[Ro_ind][A_star_ind], xticks=list(range(1, len(d_all[Ro_ind]) + 1)), xticklabels=d_all_label[Ro_ind])
         axs_Ro[Ro_ind][A_star_ind].set_ylim(0, ylim_Ro[Ro_ind])
         axs_Ro[Ro_ind][A_star_ind].set_ylabel('A* = {}'.format(A_star))
 
