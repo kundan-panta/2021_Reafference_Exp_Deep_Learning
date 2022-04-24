d808fad1d59fb7bdbc5e3b55c2c3403230c0f4a1
kundan-panta <kundan.panta123@gmail.com>
2022-04-24 01:23:42 -0400

    make it easier to switch between w2w and s2w models;

--- loss_compare.py
+++ loss_compare.py
@@ -2,7 +2,6 @@
 import numpy as np
 from os import walk
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FormatStrFormatter
 from pathlib import Path
 
 Ro_all = [2, 3.5, 5]
@@ -12,12 +11,30 @@ A_star_all = [2, 3, 4]
 losses_mean = np.full((len(Ro_all), len(A_star_all)), np.NaN)
 losses_std = np.full((len(Ro_all), len(A_star_all)), np.NaN)
 
-# assume there's only 1 folder for each case
 root_folder = ''  # include trailing slash
-save_folders = ['plots/2022.03.25_exp_best_sh=5/', 'plots/2022.03.25_exp_best_sh=50/', 'plots/2022.03.25_exp_best_sh=500/', 'plots/2022.04.14_exp_sh=5000/', 'plots/2022.04.16_exp_sh=50000/']  # include trailing slash
-# save_folders = ['plots/2022.03.26_exp_best_same_sensor_loc_sh=500/']  # include trailing slash
+plot_folder = root_folder + 'plots/2022.04.24_loss_plots/'
+
+# choose between wingtip-to-wall or sensor-to-wall models
+w2w = True
+if w2w:
+    save_folders = [
+        'plots/2022.03.25_exp_best_sh=5/',
+        'plots/2022.03.25_exp_best_sh=50/',
+        'plots/2022.03.25_exp_best_sh=500/',
+        'plots/2022.04.14_exp_sh=5000/',
+        'plots/2022.04.16_exp_sh=50000/'
+    ]  # include trailing slash
+    plot_folder += 'w2w/'
+else:
+    save_folders = [
+        'plots/2022.03.26_exp_best_same_sensor_loc_sh=500/',
+        'plots/2022.04.17_exp_best_same_sensor_loc_sh=5/',
+        'plots/2022.04.19_exp_best_same_sensor_loc_sh=50/',
+        'plots/2022.04.19_exp_best_same_sensor_loc_sh=5000/'
+    ]  # include trailing slash
+    plot_folder += 's2w/'
+
 save_folders = [root_folder + save_folder for save_folder in save_folders]
-plot_folder = root_folder + 'plots/2022.04.17_loss_plots/'
 
 # %% mean and std plots
 
@@ -67,42 +84,42 @@ def loss_mean_std(losses_case):
 # plt.ylabel('Test Set Loss')
 
 # %% boxplot
-# # distances between which to get losses
-# d_min = 0
-# d_max = 99
+# distances between which to get losses
+d_min = 0
+d_max = 99
 
-# ylim = [0, 8]
-# plt.tight_layout()
+ylim = [0, 8]
+plt.tight_layout()
 
-# fig = plt.figure(figsize=[16, 4])
-# fig.supxlabel('Ro')
-# fig.supylabel('|Prediction Error| (cm)')
+fig = plt.figure(figsize=[16, 4])
+fig.supxlabel('Ro')
+fig.supylabel('|Prediction Error| (cm)')
 
-# for A_star_ind, A_star in enumerate(A_star_all):
-#     losses_boxplot = []
-#     for Ro_ind, Ro in enumerate(Ro_all):
-#         losses_boxplot.append(load_loss(Ro, A_star, d_min, d_max))
+for A_star_ind, A_star in enumerate(A_star_all):
+    losses_boxplot = []
+    for Ro_ind, Ro in enumerate(Ro_all):
+        losses_boxplot.append(load_loss(Ro, A_star, d_min, d_max))
 
-#     plt.subplot(1, 3, A_star_ind + 1)
-#     plt.boxplot(losses_boxplot, showfliers=False, notch=True)
-#     plt.xticks([1, 2, 3], Ro_all)
-#     plt.title('A* = {}'.format(A_star))
-#     plt.ylim(ylim)
+    plt.subplot(1, 3, A_star_ind + 1)
+    plt.boxplot(losses_boxplot, showfliers=False, notch=True)
+    plt.xticks([1, 2, 3], Ro_all)
+    plt.title('A* = {}'.format(A_star))
+    plt.ylim(ylim)
 
-# fig = plt.figure(figsize=[16, 4])
-# fig.supxlabel('A*')
-# fig.supylabel('|Prediction Error| (cm)')
+fig = plt.figure(figsize=[16, 4])
+fig.supxlabel('A*')
+fig.supylabel('|Prediction Error| (cm)')
 
-# for Ro_ind, Ro in enumerate(Ro_all):
-#     losses_boxplot = []
-#     for A_star_ind, A_star in enumerate(A_star_all):
-#         losses_boxplot.append(load_loss(Ro, A_star, d_min, d_max))
+for Ro_ind, Ro in enumerate(Ro_all):
+    losses_boxplot = []
+    for A_star_ind, A_star in enumerate(A_star_all):
+        losses_boxplot.append(load_loss(Ro, A_star, d_min, d_max))
 
-#     plt.subplot(1, 3, Ro_ind + 1)
-#     plt.boxplot(losses_boxplot, showfliers=False, notch=True)
-#     plt.xticks([1, 2, 3], A_star_all)
-#     plt.title('Ro = {}'.format(Ro))
-#     plt.ylim(ylim)
+    plt.subplot(1, 3, Ro_ind + 1)
+    plt.boxplot(losses_boxplot, showfliers=False, notch=True)
+    plt.xticks([1, 2, 3], A_star_all)
+    plt.title('Ro = {}'.format(Ro))
+    plt.ylim(ylim)
 
 # %% make loss plots with multiple rows for each distance
 # d_rows = [[1, 1], [4, 4], [7, 7], [10, 10], [13, 13], [16, 16], [19, 19], [22, 22], [25, 25], [28, 28], [31, 31], [34, 34], [37, 37], [40, 40]]
@@ -154,8 +171,10 @@ def loss_mean_std(losses_case):
 # create folder to save plots to
 Path(plot_folder).mkdir(parents=True, exist_ok=True)
 
-d_all = [list(range(1, 40 + 1, 3))] * len(Ro_all)
-# d_all = [list(range(10, 46 + 1, 3)), list(range(4, 40 + 1, 3)), list(range(1, 37 + 1, 3))]  # if using same sensor-to-wall distance
+if w2w:
+    d_all = [list(range(1, 40 + 1, 3))] * len(Ro_all)
+else:
+    d_all = [list(range(10, 46 + 1, 3)), list(range(4, 40 + 1, 3)), list(range(1, 37 + 1, 3))]
 
 # find wingroot-to-wall distance from opposite wall of tank
 wing_len_Ro = [12.367, 17.059, 20.827]
@@ -165,7 +184,7 @@ for Ro_ind in range(len(d_all)):
     d_all_opp.append([round(tank_len - (wing_len_Ro[Ro_ind] + d), 1) for d in d_all[Ro_ind]])
 
 # use wingroot-to-wall instead of wingtip-to-wall in the plot
-sensor_to_wall_distance = True
+sensor_to_wall_distance = False
 
 if sensor_to_wall_distance:
     # calculate the wingroot-to-wall distance
